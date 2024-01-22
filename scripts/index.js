0c0e066501656c4cb8b0a47897fa87cccfaf9e9e
Sofya Yakovleva <sofavasiv0@gmail.com>
2024-01-22 03:08:06 +0300

    Первый

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.getElementById("card-template").content; // @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const content = document.querySelector(".content"); // @todo: DOM узлы
+const placesList = content.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function addCard(element, del) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImg = cardElement.querySelector(".card__image");
+  const delButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__title").textContent = element.name;
+  cardImg.src = element.link;
+  cardImg.alt = element.name;
+
+  delButton.addEventListener("click", del);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+  const card = event.target.closest(".card");
+  card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+  placesList.append(addCard(item, deleteCard));
+});
